47fafa9b9fa8f90b2f702bfd34145449f511d354
GiuseppeVolpe <giuseppe.volpe602@gmail.com>
2023-05-29 14:56:32 +0200

    Created WebServer logic

--- AlIve/WebServer.py
+++ AlIve/WebServer.py
@@ -1,29 +1,65 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import mysql.connector as mysqlconn
+import os
+import operator
 
-hostName = "localhost"
-serverPort = 8080
+app = Flask(__name__)
+db_connection = mysqlconn.connect(user='GiuseppeVolpe', password='password', database='alive_db')
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server. Here AlIve project will born.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+@app.route('/')
+def home():
+  if not session.get('logged_in'):
+    return render_template('login.html')
+  else:
+    return render_template('index.html')
 
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+@app.route('/signup', methods=['POST'])
+def add_user():
 
+    form = request.form
+
+    username = form['username']
+    password = form['password']
+    email = form['email']
+
+    cur = db_connection.cursor()
+    
     try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
+        cur.execute('INSERT INTO alive_users (username, user_password, email) VALUES (%s, %s, %s)', (username, password, email))
+        db_connection.commit()
+        cur.close()
+    except Exception as ex:
+        print(ex)
+        return("Couldn't add user...")
 
-    webServer.server_close()
-    print("Server stopped.")
+    return "New user added"
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+  login = request.form
+
+  userName = login['username']
+  password = login['password']
+
+  cur = db_connection.cursor(buffered=True)
+  data = cur.execute('SELECT * FROM alive_users WHERE username=%s', (userName))
+  data = cur.fetchone()[2]
+
+  if password == data:
+    account = True
+
+  if account:
+    session['logged_in'] = True
+  else:
+    flash('wrong password!')
+  return home()
+
+@app.route('logout')
+def logout():
+  session['logged_in'] = False
+  return home()
+
+if __name__ == "__main__":
+  app.secret_key = os.urandom(12)
+  app.run(debug=False,host='0.0.0.0', port=5000)
